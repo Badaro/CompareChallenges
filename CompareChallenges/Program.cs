767ca1ad4b3d6ee790ed056e8c8535cddfac632a
Filipe Badaró <badaro@gmail.com>
2023-03-15 13:32:03 -0300

    Initial commit

---
+++ CompareChallenges/Program.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace CompareChallenges
+{
+    internal class Program
+    {
+        static string _rootUrl = "https://badaro.github.io/MTGODecklistCache/Tournaments/mtgo.com";
+        static string _outputFile = "event_data.csv";
+
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Invalid number of parameters, you need to specify at least one event");
+                return;
+            }
+
+            try
+            {
+                Console.WriteLine($"Comparing cards between:");
+                args.ToList().ForEach(a => Console.WriteLine($"* {a}"));
+
+                List<Dictionary<string, int>> combinedEventData = new List<Dictionary<string, int>>();
+                args.ToList().ForEach(a => combinedEventData.Add(GetCardsFromEvent(a)));
+
+                StringBuilder output = new StringBuilder();
+                output.Append("Card,");
+                output.Append(String.Join(",", args));
+                output.Append(Environment.NewLine);
+
+                foreach (var card in combinedEventData.SelectMany(i => i.Keys).Distinct().ToList())
+                {
+                    output.Append($"\"{card}\"");
+                    foreach (var eventData in combinedEventData)
+                    {
+                        if (eventData.ContainsKey(card)) output.Append($",\"{eventData[card]}\"");
+                        else output.Append(",\"0\"");
+                    }
+                    output.Append(Environment.NewLine);
+                }
+
+                File.WriteAllText(_outputFile, output.ToString());
+                Console.WriteLine($"Result saved to {_outputFile}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error during processing: {ex.Message}");
+            }
+        }
+
+        private static Dictionary<string, int> GetCardsFromEvent(string eventName)
+        {
+            Dictionary<string, int> result = new Dictionary<string, int>();
+
+            string eventDate = String.Join("/", eventName.Split("-").TakeLast(3)).Substring(0, 10);
+            string eventUrl = $"{_rootUrl}/{eventDate}/{eventName}.json";
+            string eventData = new HttpClient().GetStringAsync(eventUrl).Result;
+
+            dynamic json = JsonConvert.DeserializeObject<dynamic>(eventData);
+            foreach (var deck in json.Decks)
+            {
+                foreach (var card in deck.Mainboard)
+                {
+                    string name = card.CardName;
+                    int count = card.Count;
+
+                    if (!result.ContainsKey(name)) result.Add(name, count);
+                    else result[name] += count;
+                }
+                foreach (var card in deck.Sideboard)
+                {
+                    string name = card.CardName;
+                    int count = card.Count;
+
+                    if (!result.ContainsKey(name)) result.Add(name, count);
+                    else result[name] += count;
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # CompareChallenges
-Quick tool I wrote for @sic at the Tron Discord to compare cards between MTGO events
+Quick tool I wrote for sic at the Tron Discord to compare cards between MTGO events and export it as a CSV file for Excel.
+
+Usage:
+
+1) Install .Net 7 Runtime: https://dotnet.microsoft.com/en-us/download/dotnet/7.0
+2) Run the tool like this:
+
+`CompareChallenges modern-challenge-2023-03-1212529561 modern-challenge-32-2023-03-1112529558 modern-showcase-challenge-2023-03-1112529524`
+
+A pre-built version if available here: https://github.com/Badaro/CompareChallenges/releases
+
+Data for this version comes from https://github.com/Badaro/MTGODecklistCache. This repository is updated daily at 17UTC with new events from MTGO, so if an event was released on the same day it won't be available until that time.
